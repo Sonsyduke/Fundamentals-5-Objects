bb096987d17b1a83e9ae4edbe98cb0169f62a9f3
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-02 10:02:15 +0200

    Finish exercise 3

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-const user = {};
+// Exercise 1
+// const user = {};
 
-user.name = "John";
-user.surname = "Smith";
-user.name = "Pete";
-delete user.name;
+// user.name = "John";
+// user.surname = "Smith";
+// user.name = "Pete";
+// delete user.name;
 
-let schedule = {};
+// Exercise 2
+// let schedule = {};
 
-function isEmpty(obj) {
-  for (let key in obj) {
-    return false;
-  }
-  return true;
-}
+// function isEmpty(obj) {
+//   for (let key in obj) {
+//     return false;
+//   }
+//   return true;
+// }
 
-console.log(isEmpty(schedule));
+// console.log(isEmpty(schedule));
 
-schedule["8:30"] = "get up";
+// schedule["8:30"] = "get up";
 
-console.log(isEmpty(schedule));
+// console.log(isEmpty(schedule));
+
+// Exercise 3
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+
+// function sumOfSalaries(obj) {
+//   let total = 0;
+//   for (let key in obj) {
+//     total += obj[key];
+//   }
+//   return total;
+// }
+
+// console.log(sumOfSalaries(salaries));
