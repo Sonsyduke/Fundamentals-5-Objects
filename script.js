8ec370949c0bd82b79d34644b93afb34dec2b563
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-02 09:55:17 +0200

    Finish exercise 2

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ user.name = "John";
 user.surname = "Smith";
 user.name = "Pete";
 delete user.name;
+
+let schedule = {};
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
+
+console.log(isEmpty(schedule));
+
+schedule["8:30"] = "get up";
+
+console.log(isEmpty(schedule));
