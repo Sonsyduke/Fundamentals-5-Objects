765f3e5dd9b62ff6248f2a0311e07e4b039e2bfd
Douglas Lombard <douglaslombard29@gmail.com>
2022-08-02 11:48:15 +0200

    Finish the first MDN exercise

--- script.js
+++ script.js
@@ -84,19 +84,29 @@
 // const myDataValue = "1.75m";
 // person[myDataName] = myDataValue;
 
-function createPerson(name) {
-  const obj = {};
-  obj.name = name;
-  obj.introduceSelf = function () {
-    console.log(`Hi!, I'm ${this.name}.`);
+// function createPerson(name) {
+//   const obj = {};
+//   obj.name = name;
+//   obj.introduceSelf = function () {
+//     console.log(`Hi!, I'm ${this.name}.`);
+//   };
+//   return obj;
+// }
+
+// const salva = createPerson("Salva");
+// salva.name;
+// salva.introduceSelf();
+
+// const frankie = createPerson("Frankie");
+// frankie.name;
+// frankie.introduceSelf();
+
+function Person(name) {
+  this.name = name;
+  this.introduceSelf = function () {
+    console.log(`Hi! I'm ${this.name}.`);
   };
-  return obj;
 }
 
-const salva = createPerson("Salva");
-salva.name;
-salva.introduceSelf();
-
-const frankie = createPerson("Frankie");
-frankie.name;
-frankie.introduceSelf();
+const salva = new Person("Salva");
+const frenkie = new Person("Frenkie");
